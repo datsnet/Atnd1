a2c556e68484085dd12205e81e6a9d3bd360903a
Daisuke Atsumi <datsumi1013@gmail.com>
2012-02-11 11:56:25 +0900

    first commit

---
+++ src/org/dyndns/datsnet/atnd02/AtndMainActivity.java
@@ -0,0 +1,19 @@
+package org.dyndns.datsnet.atnd02;
+
+import org.dyndns.datsnet.atnd1.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class AtndMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        WebView webview = (WebView)findViewById(R.id.webView1);
+        webview.loadUrl("http://atnd.org/");
+    }
+}
